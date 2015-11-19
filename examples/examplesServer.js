a2695f57b65d759c2820d65dedc1a275aa40a4a0
Kevin Glinski <kevin.glinski@inin.com>
2015-11-19 09:49:43 -0500

    updating examples to run in ghpages

--- examples/examplesServer.js
+++ examples/examplesServer.js
@@ -7,13 +7,5 @@ app.use(express.static(__dirname));
 app.use('/dist', express.static(__dirname + '/../dist'));
 app.use('/gen', express.static(__dirname + '/../gen'));
 
-app.get("/", function(req, res){
-    res.redirect("/implicit.html")
-})
-
-app.get("/oauth2/callback", function(req,res){
-    res.redirect("/authredirect.html")
-});
-
 var httpServer = http.createServer(app);
 httpServer.listen('8085');
