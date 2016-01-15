7a1b99000167eb777ed425ab984b210e28b25a9b
Kevin Glinski <kevin.glinski@inin.com>
2016-01-15 11:03:18 -0500

    kill express server

--- executeAutomationTests.sh
+++ executeAutomationTests.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 node examples/examplesServer.js &
+WEB_PID=$!
+
+sleep 5
 
 for test in ./spec/phantomjs/*.spec.js
 do
     echo "Running $test"
     phantomjs --ssl-protocol=any $test
 done
+
+kill $WEB_PID
