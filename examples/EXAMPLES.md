351e63e98925b55eecf127004348d0527574c514
Kevin Glinski <kevin.glinski@inin.com>
2016-07-05 16:58:44 -0400

    switching to browserify, fixing bugs and moving to namespaces to match client apps sdk

---
+++ examples/EXAMPLES.md
@@ -0,0 +1,1 @@
+To run these examples, start a http server in the repo root and browser to http://localhost:8080/examples/index.html
--- templates/purecloudsession.js
+++ templates/purecloudsession.js
@@ -1,3 +1,6 @@
+
+var superagent = require('superagent');
+
 //API VERSION - {{&info.version}}
 /**
   * @description {{&info.description}}
@@ -17,7 +20,7 @@ function PureCloudSession(options) {
     this.options = options;
     this.options.token = this._getToken();
     this.setEnvironment(this.options.environment);
-    this._setHashValues();
+    this._setValuesFromUrlHash();
 }
 
 /**
@@ -28,17 +31,17 @@ PureCloudSession.prototype.setEnvironment = function setEnvironment(environment)
     this.options.environment = environment || 'mypurecloud.com';
     this.apiUrl = 'https://api.' + this.options.environment;
     this.authUrl = 'https://login.' + this.options.environment;
-}
+};
 
 /**
   * @description Attempts to login with the appropriate authentication strategy
   * @returns Promise which resolves on successful authentication, otherwise rejects with an error
   **/
 PureCloudSession.prototype.login = function login() {
-    return this.options.token
-        ? this._testTokenAccess().catch(this._authenticate())
-        : this._authenticate();
-}
+    return this.options.token ? this._testTokenAccess().catch(function(){
+            this._authenticate();
+        }) : this._authenticate();
+};
 
 PureCloudSession.prototype._authenticate = function _authenticate() {
   var strategy = this.options.strategy;
@@ -47,10 +50,10 @@ PureCloudSession.prototype._authenticate = function _authenticate() {
       var token = this.options.token;
       return this._loginWithToken(token);
     case 'implicit':
-      var clientId = this.options.clientId;
+      var implicitclientId = this.options.clientId;
       var redirectUrl = this.options.redirectUrl;
       var state = this.options.state;
-      return this._loginWithOauth(clientId, redirectUrl, state);
+      return this._loginWithImplicitGrant(implicitclientId, redirectUrl, state);
     case 'client-credentials':
       var clientId = this.options.clientId;
       var clientSecret = this.options.clientSecret;
@@ -58,7 +61,7 @@ PureCloudSession.prototype._authenticate = function _authenticate() {
     default:
       throw new Error('Authentication strategy "'+strategy+'" is not supported.');
   }
-}
+};
 
 PureCloudSession.prototype._testTokenAccess = function _testTokenAccess() {
     if(this.options.strategy === 'implicit') {
@@ -66,16 +69,16 @@ PureCloudSession.prototype._testTokenAccess = function _testTokenAccess() {
       return this._makeRequest('get', checkUrl);
     }
     return Promise.resolve();
-}
+};
 
 PureCloudSession.prototype._loginWithToken = function _loginWithToken(token) {
     if(!token) {
         throw new Error('Options requires a "token" key for the "token" strategy');
     }
     return Promise.resolve();
-}
+};
 
-PureCloudSession.prototype._loginWithOauth = function(clientId, redirectUrl, state) {
+PureCloudSession.prototype._loginWithImplicitGrant = function(clientId, redirectUrl, state) {
     var query = {
         response_type: 'token',
         client_id: encodeURIComponent(clientId),
@@ -91,21 +94,21 @@ PureCloudSession.prototype._loginWithOauth = function(clientId, redirectUrl, sta
 
     var url = Object.keys(query).reduce(qs, this.authUrl + '/authorize?');
     window.location.replace(url);
-}
+};
 
-PureCloudSession.prototype._setHashValues = function setHashValues() {
+PureCloudSession.prototype._setValuesFromUrlHash = function setValuesFromUrlHash() {
     if(!(typeof window !== 'undefined' && window.location.hash)) return;
     var hash = window.location.hash
         .slice(1).split('&')
         .reduce(function(obj, pair) {
-            keyValue = pair.split('=');
+            var keyValue = pair.split('=');
             obj[keyValue[0]] = keyValue[1];
             return obj;
         }, {});
 
     if(hash.access_token) this._setToken(hash.access_token);
     if(hash.state) this.options.state = hash.state;
-}
+};
 
 PureCloudSession.prototype._loginWithClientCredentials = function(clientId, clientSecret) {
     var self = this;
@@ -119,21 +122,21 @@ PureCloudSession.prototype._loginWithClientCredentials = function(clientId, clie
         .then(function(body) {
             self._setToken(body.access_token);
         });
-}
+};
 
 PureCloudSession.prototype._getToken = function _getToken() {
     if(this.options.token) return this.options.token;
     if(this.options.storageKey && PureCloudSession.hasLocalStorage) {
         return localStorage.getItem(this.options.storageKey);
     }
-}
+};
 
 PureCloudSession.prototype._setToken = function _setToken(token) {
     this.options.token = token;
     if(this.options.storageKey && PureCloudSession.hasLocalStorage) {
         localStorage.setItem(this.options.storageKey, token);
     }
-}
+};
 
 PureCloudSession.hasLocalStorage = (function() {
     try {
@@ -152,8 +155,8 @@ PureCloudSession.prototype.logout = function logout() {
     if(PureCloudSession.hasLocalStorage) {
         this._setToken(null);
     }
-		window.location.replace(this.authUrl + "/logout");
-}
+	window.location.replace(this.authUrl + "/logout");
+};
 
 /**
   * @description Makes authenticated requests to PureCloud
@@ -168,7 +171,7 @@ PureCloudSession.prototype.makeRequest = function makeRequest(method, url, query
     return this.login().then(function() {
         return self._makeRequest(method, url, query, body);
     });
-}
+};
 
 PureCloudSession.prototype._makeRequest = function _makeRequest(method, url, query, body) {
     var bearer = 'bearer ' + this.options.token;
@@ -177,7 +180,7 @@ PureCloudSession.prototype._makeRequest = function _makeRequest(method, url, que
         .query(query)
         .send(body);
     return this._sendRequest(request);
-}
+};
 
 PureCloudSession.prototype._baseRequest = function _baseRequest(method, url) {
     method = method.toLowerCase();
@@ -189,13 +192,13 @@ PureCloudSession.prototype._baseRequest = function _baseRequest(method, url) {
         .accept('json')
         .timeout(timeout);
 
-    if (typeof module !== 'undefined' && module.exports) {
+    if (typeof window !== 'undefined' ) {
         var userAgent = 'PureCloud SDK/Javascript {{&info.version}}';
         request = request.set('User-Agent', userAgent);
     }
 
     return request;
-}
+};
 
 PureCloudSession.prototype._sendRequest = function _sendRequest(request) {
     return new Promise(function(resolve, reject) {
@@ -205,7 +208,7 @@ PureCloudSession.prototype._sendRequest = function _sendRequest(request) {
             resolve(res.body);
         });
     });
-}
+};
 
 /**
   * @description Conveinence method for making GET requests
@@ -215,4 +218,6 @@ PureCloudSession.prototype._sendRequest = function _sendRequest(request) {
   */
 PureCloudSession.prototype.get = function get(url, query) {
   return this.makeRequest('get', url, query);
-}
+};
+
+module.exports = PureCloudSession;
